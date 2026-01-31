a95367eb3c088b096055acf519a14a640cb6ba4c
yuxuan20101125 <136953175+yuxuan20101125@users.noreply.github.com>
2026-01-31 21:56:19 +0800

    uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -10,9 +10,9 @@ const pageConfig: PageConfig = {
   title: "探针",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'https://github.com/yuxuan20101125/UptimeFlare', label: 'GitHub' },
+    { link: 'https://amns.de5.net/', label: 'Blog' },
+    { link: '123@xx.sbcnm.dpdns.org', label: 'Email Me', highlight: true },
   ],
 }
 
@@ -28,7 +28,7 @@ const workerConfig: WorkerConfig = {
       // `method` should be a valid HTTP Method
       method: 'GET',
       // `target` is a valid URL
-      target: 'https://example.com',
+      target: 'https://bing.com',
       // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
       tooltip: 'This is a tooltip for this monitor',
       // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
